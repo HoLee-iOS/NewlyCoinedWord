75aad39713a8b18630e58bf3e8e094a58e48cc9d
skyler <ho971122@gmail.com>
2022-07-17 11:22:43 +0900

    신조어 검색기 설명 주석 추가

--- NewlyCoinedWord/ViewController.swift
+++ NewlyCoinedWord/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-
+//신조어 검색리스트를 열거형 타입으로 만듦
 enum NewList: String {
     case 윰차
     case 실매
@@ -40,6 +40,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    //열거형 타입으로 만든 신조어를 변수에 넣어줌
     var newText1: NewList = .윰차
     var newText2: NewList = .실매
     var newText3: NewList = .만반잘부
@@ -61,7 +62,7 @@ class ViewController: UIViewController {
         
     }
     
-    
+    //버튼의 속성을 셋팅하는 함수
     func setButton(buttonName: UIButton, buttonTitle: String) {
         buttonName.setTitle(buttonTitle, for: .normal)
         buttonName.layer.cornerRadius = 10
@@ -71,41 +72,42 @@ class ViewController: UIViewController {
         buttonName.tintColor = .black
     }
     
+    //레이블에 해당 단어에 대한 설명이 나오게 해주는 함수
     func labelName(description: String){
         descriptionLabel.text = description
     }
     
-    
+    //해시태그 버튼을 클릭하면 해당 언어에 대한 설명이 나오게 해줌
     @IBAction func button1Cliked(_ sender: UIButton) {
         
         labelName(description: "유모차의 준말")
         
     }
-    
     @IBAction func button2Clicked(_ sender: UIButton) {
         
         labelName(description: "실시간 매니저의 준말")
         
     }
-    
     @IBAction func button3Clicked(_ sender: UIButton) {
         
         labelName(description: "만나서 반가워 잘부탁해의 준말")
         
     }
-    
     @IBAction func button4Clicked(_ sender: UIButton) {
         
         labelName(description: "꾸민듯 안꾸민듯의 준말")
         
     }
     
+    //뷰화면을 탭하면 가상 키보드가 내려가게 해줌
     @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
         
         view.endEditing(true)
         
     }
     
+    //전송버튼을 클릭하면 userTextField의 내용을 스위치문을 이용하여 열거형의 모든 케이스와 비교함
+    //해당 케이스와 맞는 설명을 설명 레이블에 할당함
     @IBAction func searchButtonClicked(_ sender: UIButton) {
         
         let text = userTextField.text
@@ -127,6 +129,7 @@ class ViewController: UIViewController {
         
     }
     
+    //가상키보드의 리턴키를 클릭했을때도 위와 같은 작동을 하게 만들어줌
     @IBAction func returnKeyClicked(_ sender: UITextField) {
 
         let text = userTextField.text
